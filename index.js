1cf5ba0da9831f894da87bd7f0286cff6ca388e1
Lovely Upadhyay <lovely.dto@gmail.com>
2015-06-24 15:49:28 +0530

    Adding Mysql config

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Initial Code");
+var property = require('./properties'),
+    mysql    = require('mysql');
+
+var connection = mysql.createConnection(property.mysql);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
